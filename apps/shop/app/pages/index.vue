c59bf2b8113896e0beb543d50f35d826af2b4412
Bilal Kara <bilalkaracomtr@gmail.com>
2025-08-16 12:31:37 +0300

    feat: admin ve shop uygulamaları kurulumu tamamlandı (Nuxt 4, Docker, env dosyaları)

---
+++ apps/shop/app/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col items-center justify-center gap-4 h-screen">
+    <h1 class="font-bold text-2xl text-(--ui-primary)">TT Panel - Shop</h1>
+
+    <div class="flex items-center gap-2">
+      <UButton
+        label="Tasarım Teknesi"
+        icon="i-lucide-square-play"
+        to="https://www.tasarimteknesi.com"
+        target="_blank"
+      />
+
+      <UButton
+        label="GitHub"
+        color="neutral"
+        variant="outline"
+        icon="i-simple-icons-github"
+        to="https://github.com/bilalkaracomtr/ttpanel"
+        target="_blank"
+      />
+    </div>
+  </div>
+</template>
